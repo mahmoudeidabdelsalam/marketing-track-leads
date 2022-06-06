10f59b7beb318428f002239b7d345193b20b2237
Mahmoud Eid <mahmoud.eid@code95.com>
2022-06-06 10:14:16 +0200

    style Track Leads page

--- templates/template-page-marketing.php
+++ templates/template-page-marketing.php
@@ -5,11 +5,57 @@
  */
  get_header();
 ?>
-<?php
-  if ( have_posts() ) :
-    while (have_posts() ) : the_post(); 
-      the_content();
-    endwhile;
-  endif;
-?>
+<div class="container">
+  <div class="row">
+    <?php
+      if ( have_posts() ) :
+        while (have_posts() ) : the_post(); 
+          the_content();
+        endwhile;
+      endif;
+    ?>
+
+    <style>
+      .container,
+      .container-fluid,
+      .container-xxl,
+      .container-xl,
+      .container-lg,
+      .container-md,
+      .container-sm {
+        width: 100%;
+        padding-right: 12.5px;
+        padding-left: 12.5px;
+        margin-right: auto;
+        margin-left: auto;
+      }
+
+      @media (min-width: 576px) {
+        .container-sm, .container {
+          max-width: 540px;
+        }
+      }
+      @media (min-width: 768px) {
+        .container-md, .container-sm, .container {
+          max-width: 720px;
+        }
+      }
+      @media (min-width: 992px) {
+        .container-lg, .container-md, .container-sm, .container {
+          max-width: 960px;
+        }
+      }
+      @media (min-width: 1200px) {
+        .container-xl, .container-lg, .container-md, .container-sm, .container {
+          max-width: 1140px;
+        }
+      }
+      @media (min-width: 1400px) {
+        .container-xxl, .container-xl, .container-lg, .container-md, .container-sm, .container {
+          max-width: 1340px;
+        }
+      }
+    </style>
+  </div>
+</div>
 <?php get_footer(); ?>
